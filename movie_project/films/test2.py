b25b4a4604bd97a5b7ee0c2bfdf9c077db5bec4a
MasterVVK <158092075+MasterVVK@users.noreply.github.com>
2024-07-30 21:09:11 +0500

    Add factorial function

--- movie_project/films/test2.py
+++ movie_project/films/test2.py
@@ -1,3 +1,13 @@
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
 print("Test 2 file")
 
-	# This test
+# This test
+
+# Example usage
+print(factorial(5))  # Should print 120
